2302aa0b1561a6f1939d572e80de90434cad65c0
Vls <bestblodd@gmail.com>
2021-09-06 22:42:41 +0300

    delete first component

--- ng-test/src/app/app.module.ts
+++ ng-test/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { CommonModule } from "@angular/common"
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 import { HttpClientModule } from '@angular/common/http';
-import { DateComponent } from './date/date.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListComponent,
-    DateComponent,
   ],
   imports: [
     BrowserModule,
